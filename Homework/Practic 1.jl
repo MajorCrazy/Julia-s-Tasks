489ba309950febf4db66843559d3dcf53207002e
Nikita <113455964+MajorCrazy@users.noreply.github.com>
2023-05-10 17:44:26 +0300

    Update Practic 1.jl

--- Homework/Practic 1.jl
+++ Homework/Practic 1.jl
@@ -5,17 +5,23 @@ function gcd(a::Int, b::Int)
     return abs(a)
 end
 
-function gcdx_(a::T, b::T) where T
-    a0, b0 = a, b
-    u, v = one(T), zero(T)
+function gcdx_(a::Int, b::Int)
+    # a0, b0 = a, b
+    u, v = 1, 0
     u_, v_ = v, u
     while !iszero(b)
-        r, k = remdiv(a, b)
+        r, k = rem(a, b), div(a, b)
         a, b = b, r # r = a - k*b0
-        u, u_ = u_, u-k*u_
-        v, v_ = v_, v-k*v_
+        u, u_ = u_, u - k * u_
+        v, v_ = v_, v - k * v_
     end
+    if isnegative(a)
+        a, u, v = -a, -u, -v
+    end
+    return a, u, v
 end
+
+isnegative(a::Integer) = (a < 0)
 
 
 #include("Имя файла.jl")
