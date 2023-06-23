609a32b3037dab9df393c7e2e3f15886dbc7c0ee
Nikita <113455964+MajorCrazy@users.noreply.github.com>
2023-06-23 23:46:03 +0300

    Update Practic2.jl

--- Homework/Practic2.jl
+++ Homework/Practic2.jl
@@ -1,70 +1,66 @@
-# prac 2 
-#1
-function binpow(a::Float64, n::Float64)
-    if (n==0)
-        return 1
-    end
-    if (n%2==1)
-        return binpow(a,n-1)*a
-    else
-        b=binpow(a,n/2)
-        return b*b
-    end
+# 1. Функция, реализующая алгоритм быстрого возведения в степень
+
+function pow(a, n :: Int)   # t*a^n = const
+    t = one(a)
+    while n>0
+        if mod(n, 2) == 0
+            n/=2
+            a *= a 
+        else
+            n -= 1
+            t *= a
+        end
+    end  
+    return t
 end
 
+# 2. На база этой функции написать другую функцию, возвращающую n-ый член последовательности Фибоначчи (сложность - O(log n)).
 
-function non_rec_binpow(a::Int64, n::Int64)
-    res=1                                   
-    while n&gt;0                               
-        if n%2!=0                           
-            res*=a                          
-        end 
-        a*=a                                
-        n=div(n,2)                          
-    end
-    return res                  
+struct Matrix{T}
+    a11 :: T
+    a12 :: T
+    a21 :: T
+    a22 :: T
 end
 
+Matrix{T}() where T = Matrix{T}(zero(T), zero(T), zero(T), zero(T))
 
+Base. one(::Type{Matrix{T}}) where T = Matrix{T}(one(T), zero(T), zero(T), one(T))
 
-println(non_rec_binpow(2,5))
+Base. one(M :: Matrix{T}) where T = Matrix{T}(one(T), zero(T), zero(T), one(T))
 
-#2
-function non_rec_binpow(a::Float64, n::Float64)
-    res=1                                   
-    while n&gt;0                               
-        if n%2!=0                           
-            res*=a                          
-        end 
-        a*=a                                
-        n=div(n,2)                          
-    end
-    return res                  
+Base. zero(::Type{Matrix{T}}) where T = Matrix{T}()
+
+function Base. *(M1 :: Matrix{T}, M2 :: Matrix{T}) where T
+    a11 = M1.a11 * M2.a11 + M1.a12 * M2.a21
+    a12 = M1.a11 * M2.a12 + M1.a12 * M2.a22
+    a21 = M1.a21 * M2.a11 + M1.a22 * M2.a21
+    a22 = M1.a21 * M2.a12 + M1.a22 * M2.a22
+    Res = Matrix{T}(a11, a12, a21, a22)
+    return Res
 end
 
-function fib_num(n::Float64)
-    first_num=(1+sqrt(5))/2
-    second_num=(1-sqrt(5))/2
-    divider=sqrt(5)
-
-    ans=(non_rec_binpow(first_num,n)-non_rec_binpow(second_num,n))/divider
-    return ans
+function fibonachi(n :: Int)
+    Tmp = Matrix{Int}(1, 1, 1, 0) 
+    Tmp = pow(Tmp, n)
+    return Tmp.a11    
 end
 
-print(fib_num(6.0))
+# 3. Функция, вычисляющая с заданной точностью $\log_a x$
 
-#3
-function my_log(a, x, e)
-    z=x; t=1; y=0
-#ИНВАРИАНТ:  x = z^t * a^y
-    while z &lt; 1/a || z &gt; a || t &gt; e 
-        if z &lt; 1/a
+function log(a, x, e) # a > 1        
+    z = x
+    t = 1
+    y = 0
+    #ИНВАРИАНТ z^t * a^y = x
+    while z < 1/a || z > a || t > e 
+        if z < 1/a
             z *= a 
-            y += t 
-        elseif z &gt; a
-            z /= a 
             y -= t 
-        elseif t &gt; e
+        elseif z > a
+            z /= a
+            y += t
+        elseif t > e
             t /= 2 
             z *= z 
         end
@@ -72,98 +68,55 @@ function my_log(a, x, e)
     return y
 end
 
-println(my_log(7,17,0.00001))
+# 4. Функция, реализующая приближенное решение уравнения вида $f(x)=0$ методом деления отрезка пополам
 
-
-#4
-function bisection_method(f, a, b, tol)
-    # f - функция, уравнение f(x) = 0 которой нужно решить
-    # a, b - границы отрезка, на котором ищем решение
-    # tol - точность, с которой нужно найти решение
-    
-    while (b-a)/2 &gt; tol
-        c = (a+b)/2
-        if f(c) == 0
-            return c
-        elseif f(a)*f(c) &lt; 0
-            b = c
-        else
-            a = c
-        end
+function bisection(f::Function, a, b, epsilon)
+    if f(a)*f(b) < 0 && a < b
+        f_a = f(a)
+        #ИНВАРИАНТ: f_a*f(b) < 0
+        while b-a > epsilon
+            t = (a+b)/2
+            f_t = f(t)
+            if f_t == 0
+                return t
+            elseif f_a*f_t < 0
+                b=t
+            else
+                a, f_a = t, f_t
+            end
+        end  
+        return (a+b)/2
+    else
+        @warn("Некоректные данные")
     end
-    return (a+b)/2
 end
 
-f(x) = x^2-2
-a=0
-b=2
-tol=1e-6
-x=bisection_method(f,a,b,tol)
-println(x)
+# 5. Приближенное решение уравнения   $\cos x = x$ методом деления отрезка пополам.
 
-#5
-function bisection_method(f, a, b, tol)
-    # f - функция, уравнение f(x) = 0 которой нужно решить
-    # a, b - границы отрезка, на котором ищем решение
-    # tol - точность, с которой нужно найти решение
-    
-    while (b-a)/2 &gt; tol
-        c = (a+b)/2
-        if f(c) == 0
-            return c
-        elseif f(a)*f(c) &lt; 0
-            b = c
-        else
-            a = c
-        end
+bisection(x->cos(x)-x, 0, 1, 1e-8)
+
+# 6. Функцию, реализующую метод Ньютона приьлиженного решения уравнения вида $f(x)=0$
+
+function newton(r::Function, x, epsilon, num_max = 10)
+    dx = -r(x)
+    k=0
+    while abs(dx) > epsilon && k <= num_max
+        x += dx
+        dx = -r(x)
+        k += 1
     end
-    return (a+b)/2
-end
-
-f(x)= cos(x)-x
-a=0
-b=1
-tol=1e-6
-println(bisection_method(f,a,b,tol))
-
-#6
-function newton(r::Function, x; epsilon = 1e-8, num_max = 10)
-    dx = r(x); x += dx; k=1
-    while abs(dx) &gt; epsilon &amp;&amp; k &lt; num_max
-        dx = r(x); x += dx; k += 1
-    end
-    abs(dx) &gt; epsilon &amp;&amp; @warn(&quot;accuracy did not reached&quot;)
+    k > num_max && @warn("Требуемая точность не достигнута")
     return x
 end
 
-f(x) = 0
-root = newton(f, 1.0)
-println(root)
+# 7. Методом Ньютона приближеннное решение уравнения $\cos x = x$
 
-#7
-function newton(r::Function, x; epsilon = 1e-8, num_max = 10)
-    dx = r(x); x += dx; k=1
-    while abs(dx) &gt; epsilon &amp;&amp; k &lt; num_max
-        dx = r(x); x += dx; k += 1
-    end
-    abs(dx) &gt; epsilon &amp;&amp; @warn(&quot;accuracy did not reached&quot;)
-    return x
-end
+f(x) = cos(x) - x
 
-f(x) = cos(x)-x
-root = newton(f, 1.0)
-println(root)
+r(x) = -f(x)/(sin(x)+1)
 
-#8
-function newton(r::Function, x; epsilon = 1e-8, num_max = 10)
-    dx = r(x); x += dx; k=1
-    while abs(dx) &gt; epsilon &amp;&amp; k &lt; num_max
-        dx = r(x); x += dx; k += 1
-    end
-    abs(dx) &gt; epsilon &amp;&amp; @warn(&quot;accuracy did not reached&quot;)
-    return x
-end
+# 8. Методом Ньютона приближеннное значение какого-либо вещественного корня многочлена, заданного своими коэффициенами
 
-f(x) = x^2+5x-7
-root = newton(f, 1.0)
-println(root)
+p(x) = 6*x^5 - 23*x^4 + 12*x^2 + 86
+
+rp(x) = p(x) / (30*x^4 - 92*x^3 + 24*x)
