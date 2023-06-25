567c5939ffbd098f85d9bbf5226d6827408848a6
Nikita <113455964+MajorCrazy@users.noreply.github.com>
2023-06-25 16:10:54 +0300

    Update Practic4.jl

--- Homework/Practic4.jl
+++ Homework/Practic4.jl
@@ -12,26 +12,22 @@ function exp_partial_sum(x::Real, n::Int)
     return sum
 end
 
-println(exp_partial_sum(5.0, 6))
 
-#=
-2. Написать функцию, вычиляющую значение с машинной точностью (с
-максимально возможной в арифметике с плавающей точкой).
-=#
+# 2. Написать функцию, вычиляющую значение с машинной точностью (с
+# максимально возможной в арифметике с плавающей точкой).
 
-function exp_with_max_precision(x) ####
+
+function exp_with_max_precision(x) 
     y = 1.0
     term = 1.0
     k = 1
-    while y + term != y ######
+    while y + term != y 
         term *= x / k
         y += term
         k += 1
     end
     return y
 end
-
-println(exp_with_max_precision(5.0))
 
 #=
 3. Написать функцию, вычисляющую функцию Бесселя (обобщение функции синуса, колебание струны 
@@ -71,9 +67,9 @@ display(myPlot)
 
 
 
-#=
-4. Реализовать алгорим, реализующий обратный ход алгоритма Жордана-Гаусса
-=#
+
+# 4. Реализовать алгорим, реализующий обратный ход алгоритма Жордана-Гаусса
+
 
 using LinearAlgebra
 function shordan_gauss(A::AbstractMatrix{T}, b::AbstractVector{T})::AbstractVector{T} where T
@@ -91,21 +87,21 @@ function shordan_gauss(A::AbstractMatrix{T}, b::AbstractVector{T})::AbstractVect
     return x
 end
 
-#=
-5. Реализовать алгоритм, осуществляющий приведение матрицы матрицы к ступенчатому виду
-=#
+
+# 5. Реализовать алгоритм, осуществляющий приведение матрицы матрицы к ступенчатому виду
+
 
 function TransformToSteps!(matrix::AbstractMatrix, epsilon::Real = 1e-7)::AbstractMatrix
 	@inbounds for k ∈ 1:size(matrix, 1)
-		absval, Δk = findmax(abs, @view(matrix[k:end,k]))
+		absval, Δk = findmax(abs, @view(matrix[k:end, k]))
 
 		(absval <= epsilon) && throw("Вырожденая матрица")
 
 		Δk > 1 && swap!(@view(matrix[k,k:end]), @view(matrix[k+Δk-1,k:end]))
 
 		for i ∈ k+1:size(matrix,1)
-			t = matrix[i,k]/matrix[k,k]
-			@. @views matrix[i,k:end] = matrix[i,k:end] - t * matrix[k,k:end] # Макрос @. используется вместо того, чтобы в соответсвующей строчке каждую операцию записывать с точкой
+			t = matrix[i, k]/matrix[k, k]
+			@. @views matrix[i, k:end] = matrix[i, k:end] - t * matrix[k, k:end] # Макрос @. используется вместо того, чтобы в соответсвующей строчке каждую операцию записывать с точкой
 		end
 	end
 
@@ -114,9 +110,9 @@ end
 
 #6. Реализовать алгоритм, реализующий метод Жордана-Гаусса решение СЛАУ для произвольной невырожденной матрицы (достаточно хорошо обусловленной).
 
-#функцию sumprod можно оптимизировать, если две операции,
-#выполняемые в цикле в теле этой функции на одну трехместную операцию,
-#называемую fma :
+# Функцию sumprod можно оптимизировать, если две операции,
+# Выполняемые в цикле в теле этой функции на одну трехместную операцию,
+# Называемую fma:
 @inline function sumprod(vec1::AbstractVector{T}, vec2::AbstractVector{T})::T where T
 	s = zero(T)
 	@inbounds for i in eachindex(vec1)
@@ -144,8 +140,8 @@ end
 
 for n in 50:50:1000
 	println("Матрица порядка ",n,"×",n,":")
-	@time ReverseGauss_first!(randn(n,n),randn(n))
-	@time ReverseGauss!(randn(n,n),randn(n))
+	@time ReverseGauss_first!(randn(n, n), randn(n))
+	@time ReverseGauss!(randn(n, n), randn(n))
 	#= Выигрыш по времени от 2 до 3 раз
 	Матрица порядка 50×50:
 	0.000044 seconds (154 allocations: 58.641 KiB)
@@ -212,15 +208,12 @@ end
 
 #8. Написать функцию, возвращающую ранг произвольной прямоугольной матрицы (реализуется на базе приведения матрицы к ступенчатому виду).
 
-function rank!(matrix::AbstractMatrix{T},epsilon::Real = 1e-7) where T
+function rank!(matrix::AbstractMatrix{T}, epsilon::Real = 1e-7) where T
     TransformToSteps!(Matrix)
-    
 	i = 1
-
-    while abs(matrix[i,i]) <= epsilon
+    while abs(matrix[i, i]) <= epsilon
         i+=1
     end
-
     return i-1
 end
 
@@ -237,21 +230,8 @@ function determinant!(matrix::AbstractMatrix{T}) where T
 		if matrix[i, i] == zero(T)
 			break
 		end
-	
 		det *= matrix[i, i]
-		
 		i += 1
     end
-
     return det
 end
-
-#Проверка:
-#matrix = [1.0 2.0 3.0;1.0 6.0 9.0;-1.0 2.0 4.0]
-#values = [-1.0, 2.0, 2.0]
-#=println("--Матрица--")
-display(matrix)
-println("--Свободные члены--")
-display(values)
-println("--Обратный ход Гаусса--")
-display(ReverseGauss!(matrix,values))=#
