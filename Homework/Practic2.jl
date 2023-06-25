c6a8abdedd3230b1826d89f49966620a724d5e5d
Nikita <113455964+MajorCrazy@users.noreply.github.com>
2023-06-25 16:08:53 +0300

    Update Practic2.jl

--- Homework/Practic2.jl
+++ Homework/Practic2.jl
@@ -1,6 +1,6 @@
 # 1. Функция, реализующая алгоритм быстрого возведения в степень
 
-function pow(a, n :: Int)   # t*a^n = const
+function deg(a, n::Int)   # t*a^n = const
     t = one(a)
     while n>0
         if mod(n, 2) == 0
@@ -17,21 +17,21 @@ end
 # 2. На база этой функции написать другую функцию, возвращающую n-ый член последовательности Фибоначчи (сложность - O(log n)).
 
 struct Matrix{T}
-    a11 :: T
-    a12 :: T
-    a21 :: T
-    a22 :: T
+    a11::T
+    a12::T
+    a21::T
+    a22::T
 end
 
 Matrix{T}() where T = Matrix{T}(zero(T), zero(T), zero(T), zero(T))
 
 Base. one(::Type{Matrix{T}}) where T = Matrix{T}(one(T), zero(T), zero(T), one(T))
 
-Base. one(M :: Matrix{T}) where T = Matrix{T}(one(T), zero(T), zero(T), one(T))
+Base. one(M::Matrix{T}) where T = Matrix{T}(one(T), zero(T), zero(T), one(T))
 
 Base. zero(::Type{Matrix{T}}) where T = Matrix{T}()
 
-function Base. *(M1 :: Matrix{T}, M2 :: Matrix{T}) where T
+function Base. *(M1::Matrix{T}, M2::Matrix{T}) where T
     a11 = M1.a11 * M2.a11 + M1.a12 * M2.a21
     a12 = M1.a11 * M2.a12 + M1.a12 * M2.a22
     a21 = M1.a21 * M2.a11 + M1.a22 * M2.a21
@@ -40,9 +40,9 @@ function Base. *(M1 :: Matrix{T}, M2 :: Matrix{T}) where T
     return Res
 end
 
-function fibonachi(n :: Int)
+function fibonachi(n::Int)
     Tmp = Matrix{Int}(1, 1, 1, 0) 
-    Tmp = pow(Tmp, n)
+    Tmp = deg(Tmp, n)
     return Tmp.a11    
 end
 
